b0a603703026713b1c4bba6393f77db7571e48ec
katryana <katya.zadorozhna.00@gmail.com>
2023-06-15 20:17:01 +0300

    hw1dl

--- HW1/main.py
+++ HW1/main.py
@@ -1,19 +1,50 @@
 #Завдання №1
+name = "Katya"
+age = 18 # УЙОБІЩЕ ЗАВДАННЯ
 
 #Завдання №2
+rate = 1.68
+value = 50
+payment = rate * value
 
 #Завдання №3
+rate = 1.68
+value_day = 50
+value_night = 34
+payment = rate * value_day + rate * value_night/2
 
 #Завдання №4
 
+rate = 1.68
+value_day = 50
+value_night = 34
+#Payment for electricity for the current month
+payment = rate * value_day + rate * value_night/2
+
 #Завдання №5
+first_name = "Name"
+last_name = "Surname"
+# дуже важко було
 
 #Завдання №6
+full_name = f"{first_name} {last_name}"
+message = f"Dear {first_name}, we inform you that you have purchased a ticket to travel to the island of Mauritius. Departure June 31 of this year. Have a passport at {full_name}. We are looking forward to seeing you"
 
 #Завдання №7
+length = 10
+width = 5
+area = length * width
+show = f"With width {width} and length {length} of the room, its area is equal to {area}"
 
 #Завдання №8
+com1 = complex(-2, 3j)
+com2 = 4 + 2.1*j
+result = com1 + com2
 
 #Завдання №9
-
-#Завдання №10
+a = -2
+b = 7
+c = -6
+D = b ** 2 - 4*a*c
+x1 = (-b + D ** 0.5)/(2*a)
+x2 = (-b - D ** 0.5)/(2*a)
